d8b6f540f5c8e0462c6cbae0335335a8df394975
chi-feng <email.chi.feng@gmail.com>
2013-01-08 02:52:44 -0500

    Testing if Database::select() handles count correctly on table users.

--- index.php
+++ index.php
@@ -6,6 +6,10 @@ require_once('include/Database.php');
 $db = new Database();
 $db->connect('localhost', 'root', 'hicfneg12', 'playmas');
 
+$count = $db->select('users', 'count', '1');
+
+echo '<p>Number of rows in users: ' . $count . '</p>';
+
 success('End of execution.');
 exit();
 
