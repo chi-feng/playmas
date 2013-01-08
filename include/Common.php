8c90399ad59a43a75de99ee8079767ec45b5aa95
chi-feng <email.chi.feng@gmail.com>
2013-01-08 01:51:29 -0500

    Added pretty-printing to fatal_error() so that error messages won't fuck up the page layout.

--- include/Common.php
+++ include/Common.php
@@ -2,11 +2,11 @@
 
 function fatal_error($errname, $details) {
   echo '<div style="clear:both;padding:1em;margin:1em;border:1px solid #f00;background:#fcc;">';
-  echo '<p><strong>' . htmlspecialchars($errname) .'</strong></p>';
-  echo '<p>' . htmlspecialchars($details) . '</p>';
+  echo '<p style="font-size:120%"><strong>' . htmlspecialchars($errname) .'</strong></p>';
+  echo '<p style="font-size:90%>Details:</p>';
+  echo '<p style="font-family:monospace">' . htmlspecialchars($details) . '</p>';
   echo '</div>';
   exit();
 }
-
 
 ?>
