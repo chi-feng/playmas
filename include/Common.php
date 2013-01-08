86145f686673a4ae70526d7367cb272e0c1e6fb7
chi-feng <email.chi.feng@gmail.com>
2013-01-08 02:39:43 -0500

    Added pretty-printing of success to includes/Common.php.

--- include/Common.php
+++ include/Common.php
@@ -9,4 +9,12 @@ function fatal_error($errname, $details) {
   exit();
 }
 
+function success($message) {
+  echo '<div style="clear:both;padding:1em;margin:1em;border:1px solid #0c0;background:#cfc;">';
+  echo '<p style="font-size:120%"><strong>Success!</strong></p>';
+  echo '<p style="font-size:90%>Details:</p>';
+  echo '<p style="font-family:monospace">' . htmlspecialchars($message) . '</p>';
+  echo '</div>';
+}
+
 ?>
