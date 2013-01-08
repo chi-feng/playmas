3284726ec5f84c50f032470a125a99a0c1bce486
chi-feng <email.chi.feng@gmail.com>
2013-01-08 03:16:34 -0500

    Added routing debug info to index.php

--- index.php
+++ index.php
@@ -26,6 +26,14 @@ foreach ($result as $row) {
   echo '</tr>';
 }
 
+echo '<h2>Routing info</h2>';
+
+echo '<pre>';
+var_dump($_SERVER['REQUEST_METHOD']);
+var_dump($_SERVER['REQUEST_URI']);
+var_dump($_REQUEST);
+echo '</pre>';
+
 success('End of execution.');
 exit();
 
