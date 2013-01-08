e4c3b7f61b4275f3e00798395c03b624155c4cd0
chi-feng <email.chi.feng@gmail.com>
2013-01-08 03:22:20 -0500

    Wrapped index.php in XHTML boilerplate.

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+<link href="css/screen.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
 <?php
 
 require_once('include/Common.php');
@@ -39,3 +45,5 @@ success('End of execution.');
 exit();
 
 ?>
+</body>
+</html>
