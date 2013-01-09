ded551bd068ebad14a65b5a8b7c99f2ffdd277a6
chi-feng <email.chi.feng@gmail.com>
2013-01-08 21:16:37 -0500

    Implemented href() to turn routes into links

--- include/Common.php
+++ include/Common.php
@@ -3,6 +3,8 @@
 define('TIMEZONE', 'America/New_York');
 date_default_timezone_set(TIMEZONE);
 
+define('SITEROOT', 'http://localhost/~feng/playmas/');
+
 function fatal_error($errname, $details) {
   echo '<div class="error">';
   echo '<p><strong>' . $errname .'</strong></p>';
@@ -16,6 +18,14 @@ function success($message) {
   echo '<p><strong>Success!</strong></p>';
   echo '<p>' . $message . '</p>';
   echo '</div>';
+}
+
+function validate($value, $type) {
+  
+}
+
+function href($route, $text) {
+  return sprintf('<a href="%s">%s</a>', SITEROOT.$route, $text);
 }
 
 ?>
