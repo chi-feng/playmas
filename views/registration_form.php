fa171846cd7c815b7fc99c476427861ed6b3a193
chi-feng <email.chi.feng@gmail.com>
2013-01-09 05:14:13 -0500

    Added php to display errors when submitting and fixed some jquery

--- views/registration_form.php
+++ views/registration_form.php
@@ -1,4 +1,17 @@
-<div id="registration">
+<div id="registration" style="display:none">
+<?php
+if (isset($viewOptions)) {
+  echo '<div id="errors">';
+  if (isset($viewOptions['errors']) && count($viewOptions['errors'])) {
+    echo '<ul>';
+    foreach ($viewOptions['errors'] as $error) {
+      printf("<li>%s</li>\n", $error);
+    }
+    echo '</ul>';
+  }
+  echo '</div>';
+}
+?>
 <form id="registration-form" action="<?=route('users/new');?>" method="post">
   <label for="username">Username</label>
   <input class="textbox" type="text" name="username" autofocus /><br />
@@ -10,23 +23,25 @@
   <label for="passwordVerify">Verify Password</label>
   <input class="textbox" type="password" name="passwordVerify" /><br />
   </div>
-  <input id="submit-button" class="submit-button clean-gray" type="submit" disabled />
+  <input id="submit-button" class="submit-button clean-gray" type="submit" disabled style="display:none" />
 </form>
 </div>
 
 <script> 
 
 function validateForm() {
-  if ($('input[name="password"]').val() ==  $('input[name="passwordVerify"]').val()) {
-    $('#submit-button').removeAttr('disabled');
+  if ($('input[name="password"]').val().length > 0 && $('input[name="password"]').val() ==  $('input[name="passwordVerify"]').val()) {
+    $('#submit-button').removeAttr('disabled').fadeIn(200);
   } else {
-    $('#submit-button').attr('disabled', true);
+    $('#submit-button').attr('disabled', true).fadeOut(200);
   }
 }
 
 $(document).ready(function() {
+  $('#registration').delay(100).fadeIn(400);
   $('input[name="password"]').keypress(function() {
-    $('#password-verify').slideDown(200);
+    $('#password-verify').fadeIn(200);
+    validateForm(); 
   });
 
   $('input[name="passwordVerify"]').keyup(function() {
