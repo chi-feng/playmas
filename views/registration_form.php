fe1f0917f05610e88356d40f99684ead285bc654
chi-feng <email.chi.feng@gmail.com>
2013-01-08 21:17:46 -0500

    Added registration form view

---
+++ views/registration_form.php
@@ -0,0 +1,16 @@
+<form action="<?=href('user/new');?>" method="post">
+  <label for="username">Username</label>
+  <input type="text" name="username" /><br />
+  <label for="email">Email</label>
+  <input type="text" name="email" /><br />
+  <label for="password">Password</label>
+  <input type="password" name="password" /><br />
+  <label for="passwordVerify">Verify Password</label>
+  <input type="password" name="passwordVerify" /><br />
+  <input type="submit" />
+</form>
+  
+  
+  
+  
+  
