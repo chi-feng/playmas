4e089a409d8f53ea6ddf48e86265ac78e21dc1a0
chi-feng <email.chi.feng@gmail.com>
2013-01-09 05:24:58 -0500

    Made more buttons on the homepage

--- views/home.php
+++ views/home.php
@@ -1,3 +1,3 @@
-<p>
-<?=href('users/new', 'Register');?>
-</p>
+<a class="homepage-button" href="<?=route('test');?>">Test</a>
+<a class="homepage-button" href="<?=route('users/new');?>">Register</a>
+<a class="homepage-button" href="<?=route('login');?>">Login</a>
