de17abf95a71dc53fbd7b0148feec04531305722
chi-feng <email.chi.feng@gmail.com>
2013-01-08 20:44:54 -0500

    Implemented Views.php
    TODO: check if $view is a valid view.

---
+++ include/Views.php
@@ -0,0 +1,54 @@
+<?php
+
+class Views {
+  
+  private $output;
+  
+  public function __construct() {
+    $output = '';
+  }
+  
+  public function showView($view, $options=NULL) {
+    // TODO: check if $view is a valid view
+    ob_start();
+    require("views/{$view}.php");
+    $this->output = ob_get_contents();
+    ob_end_clean();
+  }
+  
+  public function render($format) {
+    if ($format == 'html') {
+      ob_start();
+      require('views/header.php');
+      echo $this->output;
+      require('views/footer.php');
+      ob_flush();
+    } else if ($format == 'json') {
+      echo $this->output;
+    } else {
+      fatal_error('View Error', "Unknown render format '$format'");
+    }
+  }
+  
+}
+
+
+$foo = new Foo();
+
+class UserController { 
+  
+
+  public function displayRegistrationForm() {
+    global $foo; 
+    $foo->showView('registration');
+    
+    
+  }
+  
+  public function registerUser($postData) {
+    
+  }
+  
+}
+
+?>
