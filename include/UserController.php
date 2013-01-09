fe3ebc79eaf502c149b00f4aa6a5473e4969d9d1
chi-feng <email.chi.feng@gmail.com>
2013-01-08 22:14:57 -0500

    Added UserController

---
+++ include/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+class UserController { 
+  
+  public function __construct() {
+    
+  }
+
+  public function displayRegistrationForm() {
+    global $views;
+    $views->showView('registration_form');
+  }
+  
+  public function registerUser($postData) {
+    // TODO: if valid, put into database and redirect to 
+    //       inbox otherwise, display registration form 
+    //       and show errors
+  }
+  
+  private function validateRegistration($postData) {
+    
+  }
+  
+}
+
+?>
