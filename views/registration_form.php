4203b6db0680064fc46684c53f3e1036bf1d2444
chi-feng <email.chi.feng@gmail.com>
2013-01-08 22:15:49 -0500

    Added classes to form elements for css styling

--- views/registration_form.php
+++ views/registration_form.php
@@ -1,16 +1,13 @@
-<form action="<?=href('user/new');?>" method="post">
+<div id="registration">
+<form id="registration-form" action="<?=route('users/new');?>" method="post">
   <label for="username">Username</label>
-  <input type="text" name="username" /><br />
+  <input class="textbox" type="text" name="username" /><br />
   <label for="email">Email</label>
-  <input type="text" name="email" /><br />
+  <input class="textbox" type="text" name="email" /><br />
   <label for="password">Password</label>
-  <input type="password" name="password" /><br />
+  <input class="textbox" type="password" name="password" /><br />
   <label for="passwordVerify">Verify Password</label>
-  <input type="password" name="passwordVerify" /><br />
-  <input type="submit" />
+  <input class="textbox" type="password" name="passwordVerify" /><br />
+  <input class="submitbutton clean-gray" type="submit" />
 </form>
-  
-  
-  
-  
-  
+</div>
