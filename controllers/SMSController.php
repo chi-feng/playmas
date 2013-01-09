f5282b2a34c3644954d7fb0a3930beda7cb32d8e
Jeff Liu <email.jeffrey.liu@gmail.com>
2013-01-09 12:55:31 -0500

    created SMSController.php and added function prototypes

---
+++ controllers/SMSController.php
@@ -0,0 +1,22 @@
+<?php
+
+class SMSController {
+
+  public function __construct() {
+
+  }
+
+  public function receiveSMS($incoming) {
+    //TODO: check if SMS goes to a valid account
+    //      if yes, add to database, if no: error
+
+  }
+
+  public function sendSMS($outgoing) {
+
+  }
+
+}
+
+
+?>
