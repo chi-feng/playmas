66513bc512a9cc441e1795afeaeb98fc36f558e0
chi-feng <email.chi.feng@gmail.com>
2013-01-08 22:16:19 -0500

    Added routing handling to index.php

--- index.php
+++ index.php
@@ -6,11 +6,24 @@ require_once('include/Views.php');
 $views = new Views();
 
 $action = isset($_GET['a']) ? $_GET['a'] : 'home';
+$verb = $_SERVER['REQUEST_METHOD'];
 
 if ($action == 'test') {
   $views->showView('test');
 }
 
+if ($action == 'user_new') {
+  require_once('include/UserController.php');
+  $userCtrl = new UserController(); 
+  if ($verb == 'GET') {
+    $userCtrl->displayRegistrationForm(); 
+  } elseif ($verb == 'POST') {
+    $userCtrl->registerUser($_POST);
+  } else {
+    fatal_error('Invalid Request', 'Unknown HTTP verb in user_new');
+  }
+}
+  
 $views->render('html');
 
 ?>
