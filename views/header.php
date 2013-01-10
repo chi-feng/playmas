eeebbe23fba4be3232ca8e71ea11c4f2483515f1
chi-feng <email.chi.feng@gmail.com>
2013-01-09 23:18:37 -0500

    Added header bar.

--- views/header.php
+++ views/header.php
@@ -6,5 +6,31 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 </head>
 <body>
+  <div id="header">
+    <div id="header-bar">
+      <div id="header-links">
+        <ul>
+          <li><a href="<?=route('faq');?>">FAQ</a></li>
+          <li><a href="<?=route('aww');?>">AWW</a></li>
+          <li><a href="<?=route('yiss');?>">YISS</a></li>
+        </ul>
+      </div>
+      <div id="header-util">
+        <span id="greet-username">
+          <?php
+            echo 'Hello Anonymous User';
+          ?>
+        </span>
+        <?php
+        if (isset($_SESSION['username'])) {
+          echo '<a id="logout-button" class="util-button" href="'.route('logout').'">Logout</a>';
+        } else {
+          echo '<a id="login-button" class="util-button" href="'.route('login').'">Login</a>';
+          echo '<a id="register-button" class="util-button" href="'.route('users/new').'">Register</a>';
+        }
+        ?>
+      </div>
+    </div>
+  </div>
 <div id="content">
   <h1>Play MAS</h1>
