15a02fa194ffdcfb10f2823374bc70f6d6da0a46
chi-feng <email.chi.feng@gmail.com>
2013-01-11 12:53:17 -0500

    Fixed $user->get[] to $user->get()

--- views/public_profile.php
+++ views/public_profile.php
@@ -5,8 +5,8 @@ $user = $viewOptions['user'];
   <div id="social">
     <div id="profile-picture">
       <?php
-      if ($user->get['has_picture'] == 1) {
-        $src = route('resources/profiles/') . $user->get['id'] . '.png';
+      if ($user->get('has_picture') == 1) {
+        $src = route('resources/profiles/') . $user->get('id') . '.png';
       } else {
         $src = route('resources/profiles/default.png');
       }
@@ -16,25 +16,25 @@ $user = $viewOptions['user'];
     <div id="social-networks">
       <h3>Connect</h3>
       <ul>
-        <li><a href="http://twitter.com/<?=$user->get['twitter'];?>">Twitter</a></li>
-        <li><a href="http://twitter.com/<?=$user->get['twitter'];?>">Twitter</a></li>
+        <li><a href="http://twitter.com/<?=$user->get('twitter');?>">Twitter</a></li>
+        <li><a href="http://twitter.com/<?=$user->get('twitter');?>">Twitter</a></li>
       </ul>
     </div>
   </div>
   <div id="profile-info">
     <div id="profile-name">
-      <h2><?=$user->get['display_name'];?></h2>
+      <h2><?=$user->get('display_name');?></h2>
     </div>
     <div id="profile-description">
-      <p><?=$user->get['description'];?></p>
+      <p><?=$user->get('description');?></p>
     </div>
     <div id="profile-location">
       <span>
       <?php
-      if (empty($user->get['city'])) {
-        echo $user->get['country'];
+      if ($user->get('city')) {
+        echo $user->get('country');
       } else {
-        echo  $user->get['city'], $user->get['country'];
+        echo  $user->get('city'), $user->get('country');
       }
       ?>
       </span>
