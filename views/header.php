2c0dceafcffb6bab59f8f6e5d2a8beef65f524f1
chi-feng <email.chi.feng@gmail.com>
2013-01-18 05:23:42 -0500

    Took out offending $_SESSION vars in header.php (was triggering custom error handler)

--- views/header.php
+++ views/header.php
@@ -1,5 +1,6 @@
 <?php
 if (!defined('INCLUDE_GUARD')) { header("HTTP/1.0 403 Forbidden"); die(); }
+$logged_in = isset($_SESSION['username']);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en">
