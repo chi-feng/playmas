2546cd0db539346359a7cd5792dae507c9fcf89e
chi-feng <email.chi.feng@gmail.com>
2013-01-20 03:32:35 -0500

    Added received column to requests list view

--- views/request/list.php
+++ views/request/list.php
@@ -6,11 +6,12 @@
 
 $requests = $this->data['requests'];
 
-echo '<table>';
+echo '<table>
+<tr><th>user</th><th>body</th><th>from</th><th>received</th></tr>';
 foreach ($requests as $request) {
-  printf('<tr><td>%s</td><td>%s</td><td>%s</td></tr>',
+  printf('<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>',
   '<a href="'.route('users/'.$request['username']).'">'.$request['username'].'</a>',
-  $request['body'],$request['from']);
+  $request['body'],$request['from'],date('r',$request['received']));
 }
 echo '</table>';
 
